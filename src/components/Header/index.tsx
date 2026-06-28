5f062b2c91699bd3e4cd402ccdc7f969f29a6929
mario-leandro <marioleandro2004@gmail.com>
2026-06-28 16:04:02 -0300

    a

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div className="w-full h-20 flex flex-row">
+      <div className="h-full flex flex-row">
+        {/* Logo */}
+        <div>A</div>
+        {/* Nome do projeto */}
+        <h1 className="text-2xl font-bold">Syncro</h1>
+      </div>
+    </div>
+  );
+}
--- src/components/SideBar/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <div className="w-full h-20 flex flex-row">
-      <div></div>
-    </div>
-  );
-}
