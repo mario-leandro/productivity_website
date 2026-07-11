7a0853103b41526d54eb2c68eedfa034a6703196
mario-leandro <marioleandro2004@gmail.com>
2026-07-11 13:06:52 -0300

    ajustando pagina de agenda

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -1,54 +1,126 @@
+import { Plus, Search } from "lucide-react";
+
 export default function Agenda() {
-  //   const today = new Date();
+  const today = new Date();
 
-  //   const month = today.getMonth(); // 0-11
-  //   const year = today.getFullYear();
+  const month = today.getMonth(); // 0-11
+  const year = today.getFullYear();
 
-  //   const calendar = generateCalendar(year, month);
+  const calendar = generateCalendar(year, month);
+
+  const mesAno = today.toLocaleString("pt-BR", {
+    month: "long",
+    year: "numeric",
+  });
 
   return (
     <div className="flex flex-col p-6 gap-4 overflow-y-auto">
-      {/* <CalendarHeader />
-      <CalendarDays />
-      <CalendarGrid /> */}
+      <div className="flex flex-row justify-between items-center py-6">
+        <div className="flex flex-col">
+          <p className="text-2xl font-semibold">Agenda & Calendário</p>
+          <p className="text-sm text-[var(--text-secundary)]">
+            Planeje sua rotina com bloqueio de tempo e controle inteligente de
+            conflitos
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-center items-center gap-2">
+          <div className="h-8 flex flex-row justify-center items-center gap-2 rounded-xl shadow-md border p-2">
+            <Search className="text-[var(--text-secundary)]" size={16} />
+            <input
+              type="text"
+              placeholder="Buscar..."
+              className="outline-none text-xs"
+            />
+          </div>
+
+          <div className="h-8 bg-[var(--surface)] flex flex-row justify-center items-center gap-2 rounded-xl shadow-md border">
+            <div className="h-full flex flex-row justify-center items-center rounded-xl shadow-md border px-4 py-2">
+              <p className="text-xs text-[var(--text-secundary)]">Mês</p>
+            </div>
+
+            <div className="h-full flex flex-row justify-center items-center px-4 py-2">
+              <p className="text-xs text-[var(--text-secundary)]">Semana</p>
+            </div>
+
+            <div className="h-full flex flex-row justify-center items-center px-4 py-2">
+              <p className="text-xs text-[var(--text-secundary)]">Dia</p>
+            </div>
+          </div>
+
+          <button className="h-8 bg-[var(--secundary)] text-white p-2 rounded-xl text-xs flex justify-center items-center">
+            <Plus size={16} />
+            Criar Evento
+          </button>
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row">
+        {/* Div do calendário */}
+        <div className="md:w-2/3 p-6 bg-[var(--surface)] rounded-xl">
+          {/* Cabeçalho do calendário */}
+          <div className="">
+            {/* Nome do mês e ano */}
+            <div>
+              <p className="text-xl font-semibold">{mesAno.toUpperCase()}</p>
+            </div>
+
+            {/* Botões de navegação */}
+            <div>
+              <button>Anterior</button>
+              <button>Hoje</button>
+              <button>Próximo</button>
+            </div>
+          </div>
+        </div>
+
+        {/* Div das categorias e gestor de choque de horarios */}
+        <div className="md:w-1/3">
+          {/* Div das categorias */}
+          <div></div>
+
+          {/* Gestor de choque de horarios */}
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
 
-// function generateCalendar() {
-//   const cells = [];
+function generateCalendar(year: number, month: number) {
+  const cells = [];
 
-//   const firstDay = new Date(year, month, 1).getDay();
+  const firstDay = new Date(year, month, 1).getDay();
 
-//   const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-//   const previousMonthDays = new Date(year, month, 0).getDate();
+  const previousMonthDays = new Date(year, month, 0).getDate();
 
-//   // dias do mês anterior
-//   for (let i = firstDay - 1; i >= 0; i--) {
-//     cells.push({
-//       day: previousMonthDays - i,
-//       currentMonth: false,
-//     });
-//   }
+  // dias do mês anterior
+  for (let i = firstDay - 1; i >= 0; i--) {
+    cells.push({
+      day: previousMonthDays - i,
+      currentMonth: false,
+    });
+  }
 
-//   // dias do mês atual
+  // dias do mês atual
 
-//   for (let day = 1; day <= daysInMonth; day++) {
-//     cells.push({
-//       day,
-//       currentMonth: true,
-//     });
-//   }
+  for (let day = 1; day <= daysInMonth; day++) {
+    cells.push({
+      day,
+      currentMonth: true,
+    });
+  }
 
-//   // dias do próximo mês
+  // dias do próximo mês
 
-//   while (cells.length < 42) {
-//     cells.push({
-//       day: cells.length,
-//       currentMonth: false,
-//     });
-//   }
+  while (cells.length < 42) {
+    cells.push({
+      day: cells.length,
+      currentMonth: false,
+    });
+  }
 
-//   return cells;
-// }
+  return cells;
+}
