2673c990e3bd06ba2631ead5c2d0749c0a5dc344
mario-leandro <marioleandro2004@gmail.com>
2026-07-15 11:50:17 -0300

    tela de agenda quase finalizada - falta ajustar alguns filtros

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -1,4 +1,5 @@
 import { Calendario } from "@/src/components/ui/Calendar";
+import { Card, CardContent, CardHeader } from "@/src/components/ui/Card";
 import {
   Calendar,
   ChevronLeft,
@@ -43,7 +44,7 @@ export default function Agenda() {
 
           <div className="h-8 bg-[var(--surface)] flex flex-row justify-center items-center gap-2 rounded-xl shadow-md border border-[var(--surface-four)]">
             <div className="h-full flex flex-row justify-center items-center rounded-xl shadow-md border">
-              <p className="bg-violet-500/30 text-xs text-[var(--text-secundary)] px-4 py-2 rounded-xl">
+              <p className="bg-violet-500/30 text-xs text-[var(--text)] px-4 py-2 rounded-xl">
                 Mês
               </p>
             </div>
@@ -68,17 +69,58 @@ export default function Agenda() {
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row">
+      <div className="flex flex-col md:flex-row gap-4">
         {/* Div do calendário */}
         <Calendario />
 
         {/* Div das categorias e gestor de choque de horarios */}
-        <div className="md:w-1/3">
+        <div className="md:w-1/3 flex flex-col gap-4">
           {/* Div das categorias */}
-          <div></div>
+          <Card>
+            <CardHeader className="mb-2">
+              <p className="text-sm uppercase font-semibold">Categorias</p>
+            </CardHeader>
+
+            <CardContent>
+              <div className="flex flex-col">
+                <div className="flex flex-row items-center gap-2">
+                  <span className="w-2 h-2 bg-blue-500 rounded-full"></span>
+                  <p>Trabalho</p>
+                </div>
+
+                <div className="flex flex-row items-center gap-2">
+                  <span className="w-2 h-2 bg-green-500 rounded-full"></span>
+                  <p>Pessoal</p>
+                </div>
+
+                <div className="flex flex-row items-center gap-2">
+                  <span className="w-2 h-2 bg-yellow-500 rounded-full"></span>
+                  <p>Estudos</p>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
 
           {/* Gestor de choque de horarios */}
-          <div></div>
+          <Card>
+            <CardHeader className="mb-4">
+              <p className="text-sm uppercase font-semibold mb-2">
+                Gestor de choque de horarios
+              </p>
+              <p className="text-xs text-[var(--text-secundary)]">
+                O Aura detecta automaticamente sobreposição de horários e exibe
+                alertas para manter seu dia equilibrado.
+              </p>
+            </CardHeader>
+
+            <CardContent>
+              <div className="flex flex-col">
+                <p className="text-xs text-[var(--text-secundary)] p-2 rounded-xl border border-[var(--surface-four)]">
+                  Hoje você não tem nenhum choque de horários. Esta tudo ótimo!
+                </p>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
