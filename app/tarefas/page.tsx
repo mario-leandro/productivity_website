4830ad21fbe842925ce4e1fd7bb17946572db75a
mario-leandro <marioleandro2004@gmail.com>
2026-07-20 10:41:43 -0300

    Iniciando desenvolvimento dos filtros das tarefas - lista finalizada, iniciando kanban

--- app/tarefas/page.tsx
+++ app/tarefas/page.tsx
@@ -1,4 +1,7 @@
+"use client";
 import {
+  Calendar,
+  ChevronRight,
   Funnel,
   GitCommitHorizontal,
   Kanban,
@@ -7,8 +10,37 @@ import {
   Search,
   SquareCheckBig,
 } from "lucide-react";
+import { useState } from "react";
 
 export default function Tarefas() {
+  const [activeTab, setActiveTab] = useState("Lista");
+
+  const modalTaskList = [
+    {
+      id: 1,
+      title: "Lista",
+      icon: <List size={12} />,
+      active: true,
+      component: TaskComponentList,
+    },
+    {
+      id: 2,
+      title: "Kanban",
+      icon: <Kanban size={12} />,
+      active: false,
+      component: "",
+    },
+    {
+      id: 3,
+      title: "Timeline",
+      icon: <GitCommitHorizontal size={12} />,
+      active: false,
+      component: "",
+    },
+  ];
+
+  const isActive = "bg-violet-500/30 text-[var(--text)]";
+
   return (
     <div className="flex flex-col p-6 gap-4 overflow-y-auto">
       <div className="flex flex-row justify-between items-center py-6">
@@ -22,25 +54,21 @@ export default function Tarefas() {
           </p>
         </div>
         <div className="flex flex-row justify-center items-center gap-2">
-          <div className="h-8 bg-[var(--surface)] flex flex-row justify-center items-center gap-2 rounded-xl shadow-md border border-[var(--surface-four)]">
-            <div className="h-full flex flex-row justify-center items-center rounded-xl shadow-md border">
-              <p className="flex flex-row justify-center items-center gap-1 bg-violet-500/30 text-xs text-[var(--text)] px-4 py-2 rounded-xl">
-                <List size={12} />
-                Lista
-              </p>
-            </div>
-            <div className="h-full flex flex-row justify-center items-center">
-              <p className="flex flex-row justify-center items-center gap-1 text-xs text-[var(--text-secundary)] p-2 rounded-xl">
-                <Kanban size={12} />
-                Kanban
-              </p>
-            </div>
-            <div className="h-full flex flex-row justify-center items-center">
-              <p className="flex flex-row justify-center items-center gap-1 text-xs text-[var(--text-secundary)] p-2 rounded-xl">
-                <GitCommitHorizontal size={12} />
-                Timeline
-              </p>
-            </div>
+          <div className="h-8 bg-[var(--surface)] flex flex-row justify-center items-center gap-2 rounded-xl">
+            {modalTaskList.map((item) => (
+              <div
+                key={item.id}
+                onClick={() => setActiveTab(item.title)}
+                className={`h-full flex flex-row justify-center items-center rounded-xl`}
+              >
+                <p
+                  className={`flex flex-row justify-center items-center gap-1 text-xs rounded-xl cursor-pointer p-2 ${activeTab === item.title ? isActive : "text-[var(--text-secundary)]"}`}
+                >
+                  {item.icon}
+                  {item.title}
+                </p>
+              </div>
+            ))}
           </div>
 
           <button className="h-8 bg-[var(--secundary)] text-white p-2 rounded-xl text-xs flex justify-center items-center">
@@ -102,49 +130,137 @@ export default function Tarefas() {
         </div>
       </div>
 
-      <div className="flex flex-col gap-4 bg-[var(--surface)] rounded-2xl p-6">
-        <div className="h-15 flex flex-row justify-between bg-[var(--surface-three)] border border-[var(--surface-four)] rounded-2xl">
-          <div className="flex flex-row">
-            <div className="flex flex-row justify-between items-center p-4">
-              <input type="checkbox" name="completed" id="completed" />
-              <div className="w-full flex flex-col"></div>
-            </div>
+      {activeTab === "Lista" && <TaskComponentList />}
+      {activeTab === "Kanban" && <TaskComponentKanban />}
+      {activeTab === "Timeline" && <TaskComponentTimeline />}
+    </div>
+  );
+}
 
-            <div className="flex flex-col justify-center items-start">
-              <p className="text-sm text-[var(--text)] font-semibold">
-                Fazer API do Syncro
-              </p>
-              <p className="text-xs text-[var(--text-secundary)]">
-                Integrar API do Syncro com o sistema.
-              </p>
-            </div>
+function TaskComponentList() {
+  return (
+    <div className="flex flex-col gap-4 bg-[var(--surface)] rounded-2xl p-6">
+      <div className="h-15 flex flex-row justify-between bg-[var(--surface-three)] border border-[var(--surface-four)] rounded-2xl">
+        <div className="flex flex-row">
+          <div className="flex flex-row justify-between items-center p-4">
+            <input type="checkbox" name="completed" id="completed" />
+            <div className="w-full flex flex-col"></div>
           </div>
 
-          <div className="flex flex-row gap-3 p-4 items-center">
-            <div className="flex flex-row items-center justify-center gap-1 p-1 rounded-sm border border-[var(--surface-four)]">
-              <p className="text-xs text-[var(--text-secundary)]">#Backend</p>
-            </div>
-            <div className="flex flex-row items-center justify-center gap-4 text-xs">
-              <span className="flex flex-row items-center justify-center gap-1 text-[var(--text-secundary)]">
-                <SquareCheckBig size={10} />
-                2/5
-              </span>
-              <p className="text-[var(--text-secundary)]">2026-07-18</p>
-            </div>
-            <div className="flex flex-row gap-2 justify-center items-center">
-              {/* Prioridade */}
-              <span className="bg-yellow-400/43 text-white text-xs uppercase rounded-xl p-2">
-                Média
-              </span>
+          <div className="flex flex-col justify-center items-start">
+            <p className="text-sm text-[var(--text)] font-semibold">
+              Fazer API do Syncro
+            </p>
+            <p className="text-xs text-[var(--text-secundary)]">
+              Integrar API do Syncro com o sistema.
+            </p>
+          </div>
+        </div>
 
-              {/* Status */}
-              <span className="text-xs p-2 bg-blue-500/20 border border-blue-500/40 rounded-sm">
-                A Fazer
-              </span>
+        <div className="flex flex-row gap-3 p-4 items-center">
+          <div className="flex flex-row items-center justify-center gap-1 p-1 rounded-sm border border-[var(--surface-four)]">
+            <p className="text-xs text-[var(--text-secundary)]">#Backend</p>
+          </div>
+          <div className="flex flex-row items-center justify-center gap-4 text-xs">
+            <span className="flex flex-row items-center justify-center gap-1 text-[var(--text-secundary)]">
+              <SquareCheckBig size={10} />
+              2/5
+            </span>
+            <p className="text-[var(--text-secundary)]">2026-07-18</p>
+          </div>
+          <div className="flex flex-row gap-2 justify-center items-center">
+            {/* Prioridade */}
+            <span className="bg-yellow-400/43 text-white text-xs uppercase rounded-xl p-2">
+              Média
+            </span>
+
+            {/* Status */}
+            <span className="text-xs p-2 bg-blue-500/20 border border-blue-500/40 rounded-sm">
+              A Fazer
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function TaskComponentKanban() {
+  return (
+    <div className="flex flex-row gap-4">
+      <div className="h-100 w-100 bg-[var(--surface)] border-t-4 border-blue-500 rounded-2xl">
+        <div className="flex flex-row items-center justify-between p-4">
+          <p className="flex flex-row items-center text-xs text-[var(--text)] font-semibold gap-2">
+            A Fazer
+            <Calendar size={16} />
+          </p>
+          <span className="text-xs text-[var(--text-secundary)] bg-[var(--surface-three)] w-6 h-6 flex flex-row items-center justify-center rounded-full">
+            0
+          </span>
+        </div>
+        <div className="flex flex-col gap-4 px-4">
+          <div className="flex flex-col justify-between bg-[var(--surface-three)] border border-[var(--surface-four)] rounded-2xl">
+            <div className="flex flex-col gap-2 p-3">
+              <div className="flex flex-row justify-between items-center">
+                <span className="bg-yellow-400/20 border border-yellow-400/60 text-yellow-400 text-[10px] uppercase rounded p-1">
+                  Média
+                </span>
+
+                <div className="flex flex-row">
+                  <button className="flex flex-row items-center justify-center cursor-pointer">
+                    <ChevronRight size={16} />
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex flex-col justify-center items-start">
+                <p className="text-sm text-[var(--text)] font-semibold">
+                  Fazer API do Syncro
+                </p>
+                <p className="text-xs text-[var(--text-secundary)]">
+                  Integrar API do Syncro com o sistema.
+                </p>
+              </div>
+
+              <hr className="border-[var(--surface-four)]" />
+
+              <div className="flex flex-row justify-between items-center">
+                <div className="flex flex-row items-center justify-center gap-1 px-1">
+                  <p className="text-xs text-[var(--text-secundary)]">
+                    Estudos
+                  </p>
+                </div>
+
+                <div className="flex flex-row items-center justify-center gap-1 p-1 rounded-sm">
+                  <p className="text-xs text-[var(--text-secundary)]">
+                    2026-07-20
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <div className="h-100 w-100 bg-[var(--surface)] border-t-4 border-yellow-500 rounded-2xl">
+        <div></div>
+        <div></div>
+      </div>
+      <div className="h-100 w-100 bg-[var(--surface)] border-t-4 border-purple-500 rounded-2xl">
+        <div></div>
+        <div></div>
+      </div>
+      <div className="h-100 w-100 bg-[var(--surface)] border-t-4 border-green-500 rounded-2xl">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+function TaskComponentTimeline() {
+  return (
+    <div>
+      <p>Timeline</p>
     </div>
   );
 }
