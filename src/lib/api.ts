e03239b5bf6318784a16ad3e6ba73d3fd93f8f6a
mario-leandro <marioleandro2004@gmail.com>
2026-09-24 10:49:59 -0300

    fix: removendo verificação de erro de forma incorreta

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -25,8 +25,8 @@ export async function sendRequest<T = any>(
     json = null;
   }
 
-  if (!json?.success) {
-    console.error("Request failed:", json?.message || "Unknown error");
+  if (!json) {
+    throw new Error(json?.message || json?.message || "Unknown error");
   }
 
   return json as T;
